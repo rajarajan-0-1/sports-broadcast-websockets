f7c455c37048eeb3bce627af6dfbb16cb66fc475
Rajarajan <rajarajanclg@gmail.com>
2026-02-03 06:39:25 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express, { json } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
