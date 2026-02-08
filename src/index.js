af14308d33745afd3ded498540b28c4360977dc6
Rajarajan <rajarajanclg@gmail.com>
2026-02-08 20:55:37 +0530

    implement arcjet security

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express, { json } from 'express';
 import { matchRouter } from './routes/matches.routes.js';
 import { attachWebSocketServer } from './ws/server.js';
 import http from 'http';
+import { securityMiddleware } from './arcjet.js';
 
 const app = express();
 const PORT = Number(process.env.PORT || 8000);
@@ -9,13 +10,15 @@ const HOST = process.env.HOST || '0.0.0.0';
 
 const server = http.createServer(app);
 
-
-
+app.set("trust proxy", true);
 app.use(json());
+app.use(securityMiddleware());
 
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
+
+// Apply security middleware to all routes
 
 app.use('/api/matches', matchRouter);
 
--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,6 @@
 import WebSocket, { WebSocketServer } from 'ws';
+import { wsArcjet } from '../arcjet.js';
+import { de } from 'zod/locales';
 
 function sendJSON(socket, payload) {
     if(socket.readyState !== WebSocket.OPEN) return;
@@ -8,7 +10,7 @@ function sendJSON(socket, payload) {
 
 function broadcast(wss, payload) {
     for(const client of wss.clients) {
-        if(client.readyState !== WebSocket.OPEN) return;
+        if(client.readyState !== WebSocket.OPEN) continue;
 
         client.send(JSON.stringify(payload));
     }
@@ -21,11 +23,40 @@ export function attachWebSocketServer(server) {
         payload: 1024 * 1024, // 1mb
     });
 
-    wss.on('connection', (socket) => {
+    wss.on('connection', async (socket, req) => {
+        // this if will protect the WebSocket connection with Arcjet's bot detection and rate limiting middleware
+        if(wsArcjet) {
+            try {
+                const decision = await wsArcjet.protect(req);
+                if(decision.isDenied()) {
+                    const code = decision.reason.isRateLimit() ? 1013 : 1008; // 1013 for rate limit, 1008 for other denials    
+                    const reason = decision.reason.isRateLimit() ? 'Too many connections!' : 'Forbidden!';
+                    socket.close(code, reason);
+                    return;
+                }
+            } catch (error) {
+                console.error('Error in WebSocket security check:', error);
+                socket.close(1011, 'Internal Server Error');
+                return;
+            }
+        }
+
+        socket.isAlive = true;
+        socket.on('pong', () => socket.isAlive = true);
+
         sendJSON(socket, { type: 'welcome' });
+
 
         socket.on('error', console.error);
     });
+
+    const interval = setInterval(() => {
+        wss.clients.forEach((socket) => {
+            if(socket.isAlive === false) return socket.terminate();     
+            socket.isAlive = false;
+            socket.ping();
+        });
+    }, 30000);  
 
     function broadcastMatchCreated(match) {
         broadcast(wss, { type: 'matchCreated', data: match });
